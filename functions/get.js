e8fb7e0ab332f16f939265e6bd410e85e393d232
Lee <44310445+lx200916@users.noreply.github.com>
2024-04-04 00:10:31 +0800

    Add.

--- functions/get.js
+++ functions/get.js
@@ -2,7 +2,7 @@ export async function onRequest(context) {
   //get search params
   const url = new URL(context.request.url);
   const params = url.searchParams;
-console.log(params);
+console.log(params, params.get("userId"));
 
   // get by userId
   const task = await context.env.DAKA.get(params.get("userId"));
