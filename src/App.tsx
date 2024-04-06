904a753a665b4ce0df4aa834537c614208503da4
Lee <44310445+lx200916@users.noreply.github.com>
2024-04-06 23:29:40 +0800

    Fix.

--- src/App.tsx
+++ src/App.tsx
@@ -168,7 +168,7 @@ function App() {
                 </div>
               </CardContent>
               <CardFooter>
-                <Progress value={hoursofWeek / 0.4} />
+                <Progress value={Math.min(hoursofWeek / 0.4, 1.0)} />
               </CardFooter>
             </Card>
             <Card>
@@ -184,7 +184,7 @@ function App() {
               </CardHeader>
 
               <CardFooter>
-                <Progress value={hoursofToday / (0.08 * 60)} />
+                <Progress value={Math.min(hoursofToday / (0.08 * 60), 1)} />
               </CardFooter>
             </Card>
           </div>
