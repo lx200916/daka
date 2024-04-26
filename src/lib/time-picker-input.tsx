7bc68b6f995da1343a2fefa17c68ed6027e29592
Lee <44310445+lx200916@users.noreply.github.com>
2024-04-26 22:42:29 +0800

    Fix.

--- src/lib/time-picker-input.tsx
+++ src/lib/time-picker-input.tsx
@@ -86,7 +86,31 @@ const TimePickerInput = React.forwardRef<
         setDate(setDateByType(tempDate, newValue, picker));
       }
     };
-
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const element = e.nativeEvent as any;
+      const data = element.data;
+      console.log(data);
+      if (data === "Tab") return;
+      e.preventDefault();
+      if (data === "ArrowRight") onRightFocus?.();
+      if (data === "ArrowLeft") onLeftFocus?.();
+      if (["ArrowUp", "ArrowDown"].includes(data)) {
+        const step = data === "ArrowUp" ? 1 : -1;
+        const newValue = getArrowByType(calculatedValue, step, picker);
+        if (flag) setFlag(false);
+        const tempDate = new Date(date);
+        setDate(setDateByType(tempDate, newValue, picker));
+      }
+      if (data >= "0" && data <= "9") {
+        const newValue = !flag
+          ? "0" + data
+          : calculatedValue.slice(1, 2) + data;
+        if (flag) onRightFocus?.();
+        setFlag((prev) => !prev);
+        const tempDate = new Date(date);
+        setDate(setDateByType(tempDate, newValue, picker));
+      }
+    };
     return (
       <Input
         ref={ref}
@@ -99,6 +123,7 @@ const TimePickerInput = React.forwardRef<
         value={value || calculatedValue}
         onChange={(e) => {
           e.preventDefault();
+          handleChange(e);
           onChange?.(e);
         }}
         type={type}
